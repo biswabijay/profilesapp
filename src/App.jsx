0204ce1c181e3d9e7b9b1a4fae603998a51c9aa0
JayB <78611741+biswabijay@users.noreply.github.com>
2025-04-08 19:05:59 -0400

    displaying user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +34,60 @@ function App() {
 }
 
 export default App
+*/
+
+import { useState, useEffect } from "react";
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import { generateClient } from "aws-amplify/data";
+import outputs from "../amplify_outputs.json";
+
+/**
+ * @type {import('aws-amplify/data').Client}
+ */
+Amplify.configure(outputs);
+const client = generateClient({
+  authMode: "userPool",
+});
+
+export default function App() {
+  const [userprofiles, setUserProfiles] = useState([]);
+  const { signOut } = useAuthenticator((context) => [context.user]);
+
+  useEffect(() => {
+    fetchUserProfile();
+  }, []);
+
+  async function fetchUserProfile() {
+    const { data: profiles } = await client.models.UserProfile.list();
+    setUserProfiles(profiles);
+  }
+
+  return (
+    <View>
+      <Heading level={1}>My Profile</Heading>
+      
+      <Grid templateColumns="1fr" gap="1rem">
+        {userprofiles.map((userprofile) => (
+          <Flex key={userprofile.id} direction="column">
+            <Heading level={3}>
+              {userprofile.email}
+            </Heading>
+            <Divider />
+          </Flex>
+        ))}
+      </Grid>
+      
+      <Button onClick={signOut}>Sign Out</Button>
+    </View>
+  );
+}
